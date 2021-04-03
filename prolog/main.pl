f5c0c21a6636ceabaf8bfd5e308ab6cc4446d9d7
GeovanaRamos <geovannna.1998@gmail.com>
2021-04-03 14:20:51 -0300

    [Prolog] Append zeros to binary number

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/env swipl
 
+% ENCODE
 % converte pra ascii - OK
 % de ascii para binario - OK
+% acrescenta zeros a esquerda, se necessario - OK
 % junta os binarios em grupos de 6
 % converte os grupos de 6 para o index
 % pega o base64 do index
@@ -16,13 +18,18 @@ main :-
     close(Str),
     writeln(Binary).
 
+
+% Convert ASCII list to Binary list
 codes_to_bin([], _) :- !.
-codes_to_bin([H|[]], [NewH|[]]) :- dec_to_bin(H, NewH).
+codes_to_bin([H|[]], [NewH|[]]) :- dec_to_bin(H, B), atom_length(B, L), append_zeros(B, L, NewH).
 codes_to_bin([H|T], [NewH|NewT]) :- 
-    dec_to_bin(H, NewH),  
+    dec_to_bin(H, B),
+    atom_length(B, L),
+    append_zeros(B, L, NewH),
     codes_to_bin(T, NewT).
 
 
+% Convert decimal number to binary number
 dec_to_bin(0,'0').
 dec_to_bin(1,'1').
 dec_to_bin(N,B):- 
@@ -31,3 +38,12 @@ dec_to_bin(N,B):-
     Y is N//2,
     dec_to_bin(Y,B1),
     atom_concat(B1, X, B).
+
+% Add zeros to binary number, if there are less than 8 digits
+append_zeros(B, 8, NewH) :- NewH = B.
+append_zeros(B, 7, NewH) :- atom_concat(0, B, NewH).
+append_zeros(B, L, NewH) :-
+    atom_concat(0, B, Concat),
+    NewL is L+1,
+    append_zeros(Concat, NewL, NewH).
+
