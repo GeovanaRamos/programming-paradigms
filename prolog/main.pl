281123914c10eb1604496e2bf3ca7ce11d80bca5
GeovanaRamos <geovannna.1998@gmail.com>
2021-04-03 15:42:24 -0300

    [Prolog] Group binary list into groups of 6

--- prolog/main.pl
+++ prolog/main.pl
@@ -12,11 +12,27 @@
 
 
 main :-
+    % Read file
     open('texto.txt', read, Str),
     read_stream_to_codes(Str,Codes),
-    codes_to_bin(Codes, Binary),
     close(Str),
-    writeln(Binary).
+
+    codes_to_bin(Codes, Binary),
+
+    % Group binary list into groups of 6 binary digits   
+    atomic_list_concat(Binary, BinaryConcat), % join all digits into one string
+    atom_number(BinaryConcat, BinaryConcatNumber), % convert to one big integer
+    number_codes(BinaryConcatNumber,X), % convert to list of ascii digits
+    maplist(plus(48),BinaryList,X), % convert ascii to int digits
+    
+    % check if is divisible by 6 to part, otherwise fill with zeros
+    atom_length(BinaryList, L),
+    append_zeros_2(BinaryList, L, BinaryFilled),
+
+    part(BinaryFilled, 6, BinaryGroups),
+
+    
+    writeln(BinaryGroups).
 
 
 % Convert ASCII list to Binary list
@@ -39,6 +55,7 @@ dec_to_bin(N,B):-
     dec_to_bin(Y,B1),
     atom_concat(B1, X, B).
 
+
 % Add zeros to binary number, if there are less than 8 digits
 append_zeros(B, 8, NewH) :- NewH = B.
 append_zeros(B, 7, NewH) :- atom_concat(0, B, NewH).
@@ -46,4 +63,19 @@ append_zeros(B, L, NewH) :-
     atom_concat(0, B, Concat),
     NewL is L+1,
     append_zeros(Concat, NewL, NewH).
+
+
+% Add zeros to binary list, if length is not divisible by 6
+append_zeros_2(B, L, NewB) :- L mod 6 =:= 0, B = NewB.
+append_zeros_2(B, L, NewH) :-
+    append(B, [0], Concat),
+    NewL is L+1,
+    append_zeros_2(Concat, NewL, NewH).
+
+part([], _, []).
+part(L, N, [DL|DLTail]) :-
+   length(DL, N),
+   append(DL, LTail, L),
+   part(LTail, N, DLTail).
+
 
