079c85dc57884665c31a9e03170d81c3666a6e4b
GeovanaRamos <geovannna.1998@gmail.com>
2021-04-16 19:56:16 -0300

    [Assembly] Reading file

---
+++ assembly/main.s
@@ -0,0 +1,36 @@
+section .data
+    file db "./text.txt", 0
+    len equ 1024
+
+section .bss 
+    buffer: resb 1024
+
+
+section .text
+    global _start
+
+_start:
+
+    mov ebx, file       ; saves filename
+    mov eax, 5          ; sys_open
+    mov ecx, 0          ; restart syscall
+    int 80h       
+
+    mov eax, 3          ; sys_read
+    mov ebx, eax        
+    mov ecx, buffer     
+    mov edx, len    
+    int 80h     
+
+    mov eax, 4          ; sys_write
+    mov ebx, 1          
+    mov ecx, buffer 
+    mov edx, len    
+    int 80h     
+
+    mov eax, 6          
+    int 80h     
+
+    mov eax, 1          ; sys_exit
+    mov ebx, 0 
+    int 80h
